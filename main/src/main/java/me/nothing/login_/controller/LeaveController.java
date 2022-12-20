5376c0e7c7e205a926364ff844b699cb6c363fc9
oscar <shanmon2017@gmail.com>
2022-12-20 14:21:41 +0800

    leave history work

--- main/src/main/java/me/nothing/login_/controller/LeaveController.java
+++ main/src/main/java/me/nothing/login_/controller/LeaveController.java
@@ -10,11 +10,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import me.nothing.login_.model.Leave;
 import me.nothing.login_.model._StaffDetails;
 import me.nothing.login_.service.LeaveService;
+import org.springframework.security.core.Authentication;
 
 @Controller
 @RequestMapping("/staff")
@@ -22,21 +24,20 @@ public class LeaveController {
     @Autowired
     LeaveService leaveService;
 
-    // @GetMapping("leave/history")
-    // public String employeeLeaveHistory(HttpSession session, Model model, Authentication authentication){
-    //     _StaffDetails staffDetails;
-    //     // System.out.println("start/leave/history");
-    //     // _StaffDetails usession = (_StaffDetails) session.getAttribute("usession");
-    //     // List<Leave> employeeLeave = leaveService.findLeaveWithStaffId(usession.getStaff().getStfId());
-    //     // for(Leave l: employeeLeave){
-    //     //     System.out.println("My print");
-    //     //     System.out.println(l);
-    //     // }
-    //     // System.out.println("html work");
-    //     // model.addAttribute("lhistory", employeeLeave);
-    //     // _StaffDetails staffDetails = (_StaffDetails) authentication.getPrincipal();
-    //     // Collection<? extends GrantedAuthority> authorities = staffDetails.getAuthorities();
-        
-    //     return "staff/home";
-    // }
+    @GetMapping("leave/history")
+    public String employeeLeaveHistory(HttpSession session, Model model, Authentication authentication){
+        _StaffDetails staffDetails = (_StaffDetails) authentication.getPrincipal();        
+        List<Leave> employeeLeave = leaveService.findLeaveWithStaffId(staffDetails.getStaff().getStfId());
+        for(Leave l: employeeLeave){
+            System.out.println(l);
+        }
+        model.addAttribute("lhistory", employeeLeave);
+        return "staff/home";
+    }
+
+    @GetMapping("leave/create")
+    public String newLeavePage(Model model){
+        model.addAttribute("leave", new Leave());
+        return "leave-new";
+    }
 }
